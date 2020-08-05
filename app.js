08c3d8d75abd354f472ceb92c106d15609db8f01
jongrover <hello@jonathangrover.com>
2020-08-05 18:59:42 -0400

    add solution

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var numbers, halved;
 
 // Write your solution code below here using the variables declared above.
 
+numbers = [1, 2, 3, 4, 5];
 
+halved = numbers.map((num) => num / 2);
 
 // Leave this portion of code alone to make testing work.
 
